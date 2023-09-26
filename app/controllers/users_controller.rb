e0e810d3c12b7e164fbed7cd897cf87098d6f3a0
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-09-26 16:04:56 -0700

    Completed Assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
+  before_action :authorize, only: [:show]
+
   def create
-    # byebug
-    if params[:password] === params[:password_confirmation]
-      user = User.create(user_params)
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
       render json: user, status: :created
     else
-      render json: { errors: user.errors.full_messages }, status: unprocessable_entity
+      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
     end
   end
 
@@ -13,6 +24,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.permit(:username, :password, :password_confirmation)
+  end
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
   end
 
 end
